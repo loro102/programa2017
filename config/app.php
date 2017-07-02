ef623912d32ba7c6adbcfe53de88d54e6c66808e
loro102 <loro102@gmail.com>
2017-07-02 12:42:21 +0200

    estandarizando codigo

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://programa2017.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
